f3411d52c9edde4b8bc04606148ac8d2b33add87
城千 <baoruo.br@cainiao.com>
2023-03-20 15:31:46 +0800

    test

---
+++ deployment/server_api_flask_parallel1.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from flask import request
+import time
+
+
+app = Flask(__name__)
+
+@app.route('/test', methods=['get'])
+def predict():
+    return {"msg":request.args.get("q")+"has received"}
+    
+app.run(debug=False,port=8080)
+   
